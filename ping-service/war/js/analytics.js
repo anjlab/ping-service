d2a23c93ff009542a58195604f3ad3d46dfd0187
Dmitry Gusev <dmitry.gusev@gmail.com>
2010-01-09 23:46:04 +0300

    Support for drawing line charts at analytics workspace

--- ping-service/war/js/analytics.js
+++ ping-service/war/js/analytics.js
@@ -82,4 +82,72 @@ $(document).ready(function() {
 				});
 		});
 	});
+	$(".c-m").click(function() {
+		$(".c-sm").removeClass("c-sm");
+		$(".c-sh").removeClass("c-sh");
+		$(".c-sd").removeClass("c-sd");
+
+		var $this = $(this);
+		
+		if (!$this.is(".c-n")) {
+			return;	//	not a numeric measure
+		}
+
+		var classes = $this.attr("class").split(' ');
+		var thisClass = classes[0];
+
+		var parts = thisClass.split('-');
+		var part = parts[parts.length - 1];
+
+		var columnClass = classes[classes.length - 3];
+
+		var parentClass = classes[1];
+		var offset = 0;
+		if (parentClass[0] == "x") {
+			if (parts.length - parentClass.split('-').length == 1) {
+				offset = 1;
+			}
+		}
+		
+		var $dimension = $($("." + thisClass).parent().parent().children().children().get(parts.length-2+offset));
+		
+		$dimension.addClass("c-sh");
+		
+		$("." + columnClass).each(function() {
+			var $this = $(this);
+			var classes = $this.attr("class").split(' ');
+			var thisClass = classes[0];
+			var dimensionClass = classes[classes.length-2];
+			
+			var _parts = thisClass.split('-');
+			var _part = _parts[_parts.length - 1];
+			
+			if (part == _part && parts.length == _parts.length) {
+				$this.addClass("c-sm");
+				$("#" + dimensionClass).addClass("c-sd");
+			}
+		});
+		
+   		var d = [];
+   		var ticks = [];
+   		
+   		$dimensions = $(".c-sd");
+   		$values = $(".c-sm");
+   		
+   		for (var i = 0; i < $dimensions.length; i++) {
+   			ticks.push([i, $($dimensions.get(i)).html()]);
+   			d.push([i, parseFloat($($values.get(i)).html())]);
+   		}
+
+   		$.plot($("#chart"), [{ 
+   			data: d
+   		}], 
+   		{
+   			xaxis: {
+   				ticks: ticks,
+   				autoscaleMargin: 0.02
+   			}
+   		});
+
+	});
 });
