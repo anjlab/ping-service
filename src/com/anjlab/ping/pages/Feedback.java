5eebb2e0452e22cdc5465bdcf7dd684db9fe90be
Dmitry Gusev <dmitry.gusev@gmail.com>
2012-03-11 00:00:02 +0400

    Fix null message on feedback page

--- src/com/anjlab/ping/pages/Feedback.java
+++ src/com/anjlab/ping/pages/Feedback.java
@@ -49,7 +49,9 @@ public class Feedback {
                 this.note = "Please, " + replyToReminder;
             }
         } else {
-            this.note += ".";
+            if (this.note != null) {
+                this.note += ".";
+            }
         }
     }
     
